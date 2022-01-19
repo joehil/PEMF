efae4aab0acf39c70d42e192157baae5eb1eac12
Joerg Hillebrand <hillebrand@jhillebrand.de>
2022-01-19 19:53:06 +0100

    Better

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
     "html/template"
     "net/http"
     "io/ioutil"
+    "fmt"
 )
 
 type FFiles struct {
@@ -13,7 +14,13 @@ type FFiles struct {
 type FormsData struct {
     Title string
     Frfiles []FFiles
-    Success bool
+    Stage string
+}
+
+type Answer struct {
+    Frmethod string
+    Frfile string
+    Stage string
 }
 
 func listDir(direc string) (trfiles []FFiles) {
@@ -33,24 +40,22 @@ func main() {
     tmpl := template.Must(template.ParseFiles("forms.html"))
 
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        if r.Method != http.MethodPost {
-            data := FormsData{
-		Title: "Kein Titel", 
-            	Frfiles: listDir("data/FY2300"),
-		Success: false,
-	    }
-            tmpl.Execute(w, data)
-            return
+
+        answer := Answer{
+            Frmethod: r.FormValue("frmethod"),
+            Frfile: r.FormValue("frfile"),
+            Stage: r.FormValue("stage"),
         }
 
+	fmt.Println(answer)
+
         data := FormsData{
-	    Title: "Titel",
-            Frfiles: []FFiles{
-                {Frfile: "Audio1"},
-                {Frfile: "Audio2"},
-            },
-            Success: true,
+		Title: "Kein Titel", 
+            	Frfiles: listDir("data/"+answer.Stage),
+		Stage: "Run",
         }
+
+	fmt.Println(data)
 
         // do something with details
 //        _ = details
